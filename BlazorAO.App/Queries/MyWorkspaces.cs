dd2a0815a81dc85fe250a73b7eb05f067a1976d4
Adam O'Neil <adamosoftware@gmail.com>
2021-03-20 20:55:45 -0400

    workspace selection on profile

---
+++ BlazorAO.App/Queries/MyWorkspaces.cs
@@ -0,0 +1,28 @@
+﻿using BlazorAO.Models;
+using Dapper.QX.Abstract;
+using Dapper.QX.Interfaces;
+using System.Collections.Generic;
+
+namespace BlazorAO.App.Queries
+{
+    public class MyWorkspaces : TestableQuery<Workspace>
+    {
+        public MyWorkspaces() : base(
+            @"SELECT 
+                [ws].*
+            FROM
+                [dbo].[Workspace] [ws]
+                INNER JOIN [dbo].[WorkspaceUser] [wu] ON [ws].[Id]=[wu].[WorkspaceId]
+            WHERE
+                [wu].[UserId]=@userId")
+        {
+        }
+
+        public int UserId { get; set; }
+
+        protected override IEnumerable<ITestableQuery> GetTestCasesInner()
+        {
+            yield return new MyWorkspaces();
+        }
+    }
+}
