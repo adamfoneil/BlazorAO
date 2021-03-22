b984d3c6088ea8a140261aae10df9a8efe3ad017
Adam O'Neil <adamosoftware@gmail.com>
2021-03-22 09:25:04 -0400

    setup clients

---
+++ BlazorAO.App/Queries/MyClients.cs
@@ -0,0 +1,25 @@
+﻿using BlazorAO.Models;
+using Dapper.QX.Abstract;
+using Dapper.QX.Attributes;
+using Dapper.QX.Interfaces;
+using System.Collections.Generic;
+
+namespace BlazorAO.App.Queries
+{
+    public class MyClients : TestableQuery<Client>
+    {
+        public MyClients() : base("SELECT * FROM [dbo].[Client] WHERE [WorkspaceId]=@workspaceId {andWhere} ORDER BY [Name]")
+        {
+        }
+
+        public int WorkspaceId { get; set; }
+
+        [Where("[IsActive]=@isActive")]
+        public bool? IsActive { get; set; } = true;
+
+        protected override IEnumerable<ITestableQuery> GetTestCasesInner()
+        {
+            yield return new MyClients();
+        }
+    }
+}
