df6ba9ad5409925bdbc7624b8c315c03b06d2af8
Adam O'Neil <adamosoftware@gmail.com>
2021-03-24 15:56:37 -0400

    work types

---
+++ BlazorAO.App/Queries/MyWorkTypes.cs
@@ -0,0 +1,25 @@
+﻿using BlazorAO.Models;
+using Dapper.QX.Abstract;
+using Dapper.QX.Attributes;
+using Dapper.QX.Interfaces;
+using System.Collections.Generic;
+
+namespace BlazorAO.App.Queries
+{
+    public class MyWorkTypes : TestableQuery<WorkType>
+    {
+        public MyWorkTypes() : base("SELECT * FROM [dbo].[WorkType] [wt] WHERE [WorkspaceId]=@workspaceId {andWhere} ORDER BY [Name]")
+        {
+        }
+
+        public int WorkspaceId { get; set; }
+
+        [Where("[wt].[IsActive]=@isActive")]
+        public bool? IsActive { get; set; } = true;
+
+        protected override IEnumerable<ITestableQuery> GetTestCasesInner()
+        {
+            yield return new MyWorkTypes() { WorkspaceId = 1 };
+        }
+    }
+}
