814e0d670818bf3fd2822a40f1dfe1d7e061a9b0
Adam O'Neil <adamosoftware@gmail.com>
2021-03-26 10:11:33 -0400

    removed obsolete cols RecurringTaskType.DateAddInterval and DateAddNumber

--- BlazorAO.Models/RecurringTaskType.cs
+++ BlazorAO.Models/RecurringTaskType.cs
@@ -21,23 +21,12 @@ namespace BlazorAO.Models
         /// open iconic image
         /// </summary>
         [MaxLength(50)]
-        public string Icon { get; set; }
-
-        /// <summary>
-        /// T-SQL DATEADD interval default argument
-        /// </summary>
-        [MaxLength(10)]
-        public string DateAddInterval { get; set; }
-
-        /// <summary>
-        /// T-SQL DATEADD default number argument
-        /// </summary>
-        public int DateAddNumber { get; set; }
+        public string Icon { get; set; }        
 
         public static IEnumerable<RecurringTaskType> SeedData => new RecurringTaskType[]
         {
-            new RecurringTaskType() { Name = "Create Invoices", Url = "/Invoice", DateAddInterval = "m", DateAddNumber = 1, Icon = "oi-dollar" },
-            new RecurringTaskType() { Name = "Approve Hours", Url = "/ApproveHours", DateAddInterval = "wk", DateAddNumber = 2, Icon = "oi-clipboard" }
+            new RecurringTaskType() { Name = "Create Invoices", Url = "/Invoice", Icon = "oi-dollar" },
+            new RecurringTaskType() { Name = "Approve Hours", Url = "/ApproveHours", Icon = "oi-clipboard" }
         };
     }
 }
