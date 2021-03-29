3faec94ed5302999e695a3c67b8f3bae9f5bf400
Adam O'Neil <adamosoftware@gmail.com>
2021-03-29 10:54:22 -0400

    EF samples

--- BlazorAO.App/Data/ApplicationDbContext.cs
+++ BlazorAO.App/Data/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BlazorAO.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace BlazorAO.App.Data
 {
@@ -8,6 +11,33 @@ namespace BlazorAO.App.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
+        }
+
+        public DbSet<Job> Job { get; set; }
+        public DbSet<Workspace> Workspace { get; set; }
+        public DbSet<Client> Client { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Job>()                
+                .HasKey(
+                    nameof(BlazorAO.Models.Job.ClientId), 
+                    nameof(BlazorAO.Models.Job.Name));
+
+            builder.Entity<Client>()
+                .HasKey(
+                    nameof(BlazorAO.Models.Client.WorkspaceId),
+                    nameof(BlazorAO.Models.Client.Name));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.LogTo((s) => Debug.WriteLine(s), LogLevel.Information);
         }
     }
 }
--- BlazorAO.Models/Workspace.cs
+++ BlazorAO.Models/Workspace.cs
@@ -1,7 +1,5 @@
 ﻿using BlazorAO.Models.Conventions;
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorAO.Models
 {
